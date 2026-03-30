8736ce14b4796c335c65ea08628c0aec3c6d6c56
juyeongMoon888 <whoor2828@gmail.com>
2026-03-30 23:00:42 +0900

    [Silver IV] Title: 30, Time: 168 ms, Memory: 16488 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = br.readLine();
+        int sum = 0;
+        boolean hasZero = false;
+
+        for (int i = 0; i < n.length(); i++) {
+            char c = n.charAt(i);
+
+            if (c == '0') {
+                hasZero = true;
+            }
+
+            sum += c - '0'; // ① 문자 숫자를 실제 정수로 바꾸고 ② 계산
+        }
+
+        if (!hasZero || sum % 3 != 0) {
+            System.out.println(-1);
+            return; // main() 종료
+        }
+
+        // 내림차순 정렬
+        char[] arr = n.toCharArray(); // 문자 배열로 변환
+        Arrays.sort(arr); // 오름차순
+
+        StringBuilder sb = new StringBuilder(new String(arr));
+        sb.reverse();
+
+        System.out.println(sb);
+    }
+}
